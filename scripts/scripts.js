2f2bc9c59bedb77d56b37124e76a4fdfc76e0f0d
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-02-26 18:02:38 -0600

    commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,17 @@
 $(function () {
     $("#footer").load("templates/footer.html");
 });
+
+$(function () {
+    $("#navigation").load("templates/navigation.html");
+});
+
+// Hamburger Menu
+$('.menu-btn').on('click', function (e) {
+    $(".menu-btn").toggleClass("active");
+    $(".bars .bar").toggleClass("rotated");
+    $(".header img").toggleClass("light");
+    $(".navigation").toggleClass("active");
+    $(".navigation ul li").toggleClass("active");
+    $("body").toggleClass("overflow-hidden");
+});
