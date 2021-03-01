dd9ba3f1fb1c4b96eb849213d200ba6860f2e976
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-03-01 12:10:21 -0600

    commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,27 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    freeMode: true,
+    resistanceRatio: 0,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 $(function () {
     $("#footer").load("templates/footer.html");
 });
